2c1c4695e964727d8c4d8bb32a3121470a3866b5
amustafiz <munikamustafiz89@gmail.com>
2023-03-30 16:33:31 -0400

    add helper for select

---
+++ options.ts
@@ -0,0 +1,9 @@
+export const Water = ["frequent", "average", "minimum", "none"];
+export const Cycle = ["perennial", "annual", "biennial", "Biannual"];
+export const Difficulty = ["easy", "medium", "hard"];
+export const Sunlight = [
+  "full shade",
+  "part shade",
+  "indirect light",
+  "full sun",
+];
