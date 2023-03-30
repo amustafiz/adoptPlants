384692bbfd0ea0ff9d4bcc7133b1b52dea6a2ef8
amustafiz <munikamustafiz89@gmail.com>
2023-03-30 15:57:20 -0400

    add types for posts

---
+++ types.ts
@@ -0,0 +1,36 @@
+export const Sunlight = {
+  "full shade": "full shade",
+  "part shade": "part shade",
+  "indirect light": "indirect light",
+  "full sun": "full sun",
+} as const;
+
+export const Cycle = {
+  perennial: "perennial",
+  annual: "annual",
+  biennial: "biennial",
+  biannual: "biannual",
+} as const;
+
+export const Water = {
+  frequent: "frequent",
+  average: "average",
+  minumum: "minumum",
+  none: "none",
+} as const;
+
+export interface Ad {
+  _id?: string;
+  authorId: number | string;
+  title: string;
+  description: string;
+  imageUrl: string;
+  watering: keyof typeof Water;
+  sunlight: keyof typeof Sunlight;
+  email: string;
+  indoor: boolean;
+  edible: boolean;
+  poisonous: boolean;
+  cycle: keyof typeof Cycle;
+  authorName: string;
+}
